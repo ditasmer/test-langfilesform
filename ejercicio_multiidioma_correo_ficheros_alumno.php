4559ed03f349ebdc125072c2b6c9f97d1a829f33
ditasmer <ditasmer@gmail.com>
2020-08-10 09:42:49 +0200

    First commit

---
+++ ejercicio_multiidioma_correo_ficheros_alumno.php
@@ -0,0 +1,45 @@
+﻿<?php
+
+    
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<div class="contenedor">
+    <header>
+      <h2>Exercici 3 UF1844</h2>
+    </header>
+    <nav>
+      <a href="#">Inici</a><a href="#">Productes</a><a href="#">Contacte</a>
+    </nav>
+	<section>
+		<form name='formulario' method='post' action='#'>
+			<label>Nom</label>
+			<input type='text' name='nombre'><br><br><br>
+			<label>Correu</label>
+            <input type='email' name='correo'><br><br><br>
+            <label>Missatge</label>
+			<textarea name='mensaje'></textarea><br><br><br>
+			<center>
+				<input type='submit' value='Enviar' name='enviar'><br><br>
+			</center><br><br>
+		</form>
+  		<article>
+  			<h1>SECCIÓ DE CONTINGUT</h1>
+            <img src="img/bombilla.jpg">
+            <p>Aquest text s'hi troba en català i s'hi te que mostrar als dos idiomes que hi ha seleccionables a la pàgina</p>
+  		</article>
+	</section>
+	<section>
+		<table>
+			<tr><th id='rem'>Remitent</th><th id='cor'>Correu</th><th id='msg'>Missatge</th><tr>
+		</table>
+	</section>
+	</div>
+</body>
+</html>
