7413cda750466f1f676cd76ec182d9207780c7ea
ditasmer <ditasmer@gmail.com>
2020-08-10 12:21:46 +0200

    valida campos del formulario

--- ejercicio_multiidioma_correo_ficheros_alumno.php
+++ ejercicio_multiidioma_correo_ficheros_alumno.php
@@ -1,6 +1,36 @@
 ﻿<?php
 //gestion del idioma
 require('includes/language.php');
+
+//VALIDA FORMULARIO
+//ini vars
+$correo_form = '';
+$nombre_form = '';
+$mensaje_form = '';
+$error = '';
+//comprobamos que exise Enviar 
+if(isset($_POST['enviar'])){
+	//si existe, leemos y evaluamos nombre, correo, mensaje
+	$nombre_form = trim($_POST['nombre_form']);
+	$correo_form = trim($_POST['correo_form']);
+	$mensaje_form = trim($_POST['mensaje_form']);	
+	//validar try catch datos
+	try{
+		if($nombre_form == ''){
+			throw new Exception("Nombre obligatorio", 1);
+		}
+		if($correo_form == ''){
+			throw new Exception("Correo obligatorio", 1);
+		}
+		if($mensaje_form == ''){
+			throw new Exception("Mensaje obligatorio", 1);	
+		}
+	} catch(Exception $e){
+		//tratamiento de errores
+		$error = $e->getMessage();
+	}
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,11 +48,13 @@ require('includes/language.php');
 	<section>
 		<form name='formulario' method='post' action='#'>
 			<label><?=$nombre?></label>
-			<input type='text' name='nombre'><br><br><br>
+			<input type='text' name='nombre_form' /><br><br><br>
 			<label><?=$correo?></label>
-            <input type='email' name='correo'><br><br><br>
+            <input type='email' name='correo_form' /><br><br><br>
             <label><?=$mensaje?></label>
-			<textarea name='mensaje'></textarea><br><br><br>
+			<textarea name='mensaje_form' /></textarea><br><br><br>
+			<!--zona de mensajes-->
+			<span><?=$error?></span><br><br>
 			<center>
 				<input type='submit' value='<?=$enviar?>' name='enviar'><br><br>
 			</center><br><br>
